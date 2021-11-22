e3b1d025ffae4b71ce268380f36b8396c4b714cf
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-23 04:20:58 +0700

    Challenge (convert string to slug)

--- src/StringTransformation.js
+++ src/StringTransformation.js
@@ -16,4 +16,11 @@ const _ = require("lodash");
 // console.log(_.chain("foo/bar").split("/").join(" dan ").value());
 
 //////// 3
-// Challenge (converting string)
+// Challenge (converting string to slug)
+function convertStringToSlug(str) {
+  let slug = _.chain(str).toLower().split(" ").join("-").value();
+  return encodeURI(slug);
+}
+console.log(convertStringToSlug("Kota Trenggalek sangat indah"));
+console.log(convertStringToSlug("Soal CPNS ini akan membuatmu sakit kepala"));
+console.log(convertStringToSlug("Aldino Alung Putra Anugraha"));
