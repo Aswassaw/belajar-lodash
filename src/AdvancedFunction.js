a71be2db66718738fcff067f83e9ede461ac4641
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-23 06:02:37 +0700

    Compact untuk menghilangkan item dari array yang falsy

--- src/AdvancedFunction.js
+++ src/AdvancedFunction.js
@@ -16,15 +16,38 @@ const _ = require("lodash");
 
 //////// 3
 // Flatten (mengubah array multidimensi menjadi hanya 1 dimensi)
-console.log(
-  _.flatten([
-    [1, 2, 3],
-    [4, 5, 6],
-  ])
-);
-console.log(
-  _.flattenDeep([
-    [1, 2, 3],
-    [4, 5, [6, 7, [9, 10, [11, 12, 13]]]],
-  ])
-);
+// console.log(
+//   _.flatten([
+//     [1, 2, 3],
+//     [4, 5, 6],
+//   ])
+// );
+// console.log(
+//   _.flattenDeep([
+//     [1, 2, 3],
+//     [4, 5, [6, 7, [9, 10, [11, 12, 13]]]],
+//   ])
+// );
+
+//////// 4
+// Compact (menghilangkan nilai falsy dari array)
+let arr = [1, 2, null, 3, undefined, 0, false, ""];
+console.log(_.compact(arr));
+
+let user = [
+  {
+    id: 1,
+    name: "Andry",
+  },
+  null,
+  {
+    id: 2,
+    name: "Bagas",
+  },
+  undefined,
+  {
+    id: 3,
+    name: "Adi",
+  },
+];
+console.log(_.compact(user));
