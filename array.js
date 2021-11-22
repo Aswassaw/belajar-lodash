e5be02a73442e138b00af07b60b5ad4cfbefa73e
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-22 08:29:57 +0700

    Menggunakan map di lodash untuk mapping array, object, dan array of object

--- array.js
+++ array.js
@@ -14,26 +14,84 @@ const _ = require("lodash");
 // console.log(eachStandard, eachLodash);
 
 //////// 2
-let items = {
-  1: {
-    name: "Andry Pebrianto",
-  },
-  satu: {
-    name: "Bagas Ihwaluddin",
-  },
-};
+// let items = {
+//   1: {
+//     name: "Andry Pebrianto",
+//   },
+//   satu: {
+//     name: "Bagas Ihwaluddin",
+//   },
+// };
 
 // Perulangan object dengan js native
-for (let item in items) {
-  console.log(items[item].name + "-" + item);
-}
+// for (let item in items) {
+//   console.log(items[item].name + "-" + item);
+// }
 
-let arr = [];
+// let arr = [];
 // Perulangan object dengan lodash
-_.each(items, (item, index) => {
-  console.log(item.name + "-" + index);
+// _.each(items, (item, index) => {
+//   console.log(item.name + "-" + index);
 
-  arr.push(item);
-});
+//   arr.push(item);
+// });
 
-console.log(arr);
+// console.log(arr);
+
+//////// 3
+// Standard Map
+// let newArr = [1, 2, 3].map((item) => {
+//   return item + 1;
+// });
+// console.log(newArr);
+
+// Lodash Map
+// let newArrLodash = _.map([1, 2, 3], (item) => {
+//   return item + 2;
+// });
+// console.log(newArrLodash);
+
+//////// 4
+// let users = {
+//   admin: {
+//     name: "Andry Pebrianto",
+//     age: 18,
+//   },
+//   user: {
+//     name: "Bagas Ihwaludin",
+//     age: 17,
+//   },
+// };
+// let usernames = [];
+// let ages = [];
+// let usersWithAgePlus10 = [];
+
+// Lodash Map for object
+// usernames = _.map(users, (item) => item.name);
+// ages = _.map(users, (item) => item.age);
+// usersWithAgePlus10 = _.map(users, (item, index) => {
+//   console.log(index + "-" + item.name);
+//   return {
+//     ...item,
+//     age: item.age + 10,
+//   };
+// });
+// console.log(usernames);
+// console.log(ages);
+// console.log(usersWithAgePlus10);
+
+//////// 5
+// Menggunakan array of object dan sintaks yang lebih sederhana
+let users = [
+  {
+    id: 1,
+    name: "Aswassaw",
+  },
+  {
+    id: 2,
+    name: "Fliger",
+  },
+];
+
+let usersName = _.map(users, "id");
+console.log(usersName);
