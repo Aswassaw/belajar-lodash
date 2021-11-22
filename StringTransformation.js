9e0a30e2b14f92530b8e79d79d7e1ef1d4d5d15f
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-23 04:14:01 +0700

    Split dan Join

--- StringTransformation.js
+++ StringTransformation.js
@@ -1,10 +1,16 @@
 const _ = require("lodash");
 
 //////// 1
-// Change string to uppercase/lowercase in lodash
-console.log(_.upperCase("manusia"));
-console.log(_.lowerCase("MANUSIA"));
-console.log(_.toUpper("manusia"));
-console.log(_.toLower("MANUSIA"));
-// Satu-satunya alasan untuk menggunakan fungsi case milik lodash adalah karena bisa digunakan dalam chain
-console.log(_.chain("manusia").upperCase().value());
+// // Change string to uppercase/lowercase in lodash
+// console.log(_.upperCase("manusia"));
+// console.log(_.lowerCase("MANUSIA"));
+// console.log(_.toUpper("manusia"));
+// console.log(_.toLower("MANUSIA"));
+// // Satu-satunya alasan untuk menggunakan fungsi case milik lodash adalah karena bisa digunakan dalam chain
+// console.log(_.chain("manusia").upperCase().value());
+
+//////// 8
+// Join (menggabungkan array menjadi string) & Split (memisah string menjadi array)
+console.log(_.join(["A", "n", "d", "r", "y"], ""));
+console.log(_.split("Andry", ""));
+console.log(_.chain("foo/bar").split("/").join(" dan ").value());
